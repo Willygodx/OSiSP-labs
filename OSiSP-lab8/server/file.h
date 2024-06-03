ff4f48f7ce55132d86df539d814b1eae9fc1e3a8
Ruslan Alhava <enotland34@yandex.ru>
2024-06-03 19:03:52 +0300

    edited

--- OSiSP-lab8/server/file.h
+++
@@ -1,8 +0,0 @@
-#ifndef OSISP_LAB8_FILE_H
-#define OSISP_LAB8_FILE_H
-
-#include <stddef.h>
-
-void execute_file_args(const char *__filename, char **__buffer, size_t *__size);
-
-#endif //OSISP_LAB8_FILE_H
--- OSiSP-lab8/server/network.h
+++
@@ -1,8 +0,0 @@
-#ifndef OSISP_LAB8_NETWORK_H
-#define OSISP_LAB8_NETWORK_H
-
-#include <netdb.h>
-
-void init_server(const char *port, struct addrinfo **result);
-
-#endif //OSISP_LAB8_NETWORK_H
